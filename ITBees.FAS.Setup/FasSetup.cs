50dfa64bb7ab93c8ca9cac92657afaa1c971dfc2
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-12-11 13:48:09 +0100

    Unify guid keys in aspnet user creations, and include roles in myAccount

--- ITBees.FAS.Setup/FasSetup.cs
+++ ITBees.FAS.Setup/FasSetup.cs
@@ -11,7 +11,7 @@ public class FasSetup
     /// <summary>
     /// That will scan all assemblyies in project and search for classes that implements IFasDependencyRegistration, and execute all dependencies
     /// </summary>
-    public static void RegisterAllFasDependencies<YourDbContext, YourIdentityUser>(IServiceCollection services, IConfigurationRoot configurationRoot) where YourDbContext : DbContext where YourIdentityUser : IdentityUser, new()
+    public static void RegisterAllFasDependencies<YourDbContext, YourIdentityUser>(IServiceCollection services, IConfigurationRoot configurationRoot) where YourDbContext : DbContext where YourIdentityUser : IdentityUser<Guid>, new()
     {
         var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
